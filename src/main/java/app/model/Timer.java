d368c26f9f98d6d0040872dd2bc12a2016117276
NatalieGuseva <gusberd81@gmail.com>
2024-12-04 23:16:29 +0500

    Task executed

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,6 +1,16 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Timer {
+
+
+    @Autowired
+    @Qualifier("animalsCage")
+    private AnimalsCage animalsCage;
 
     private Long nanoTime = System.nanoTime();
 
